367907fbdd6905a8ae1e3384c1d8f349199044eb
alievaz06 <seyitalievaz@gmail.com>
2026-08-01 16:00:40 -0400

    implement mazda cpp file
    implement mazda cpp file

---
+++ Mazda.cpp
@@ -0,0 +1,14 @@
+#include "Mazda.h"
+void Mazda::draw() // еhis function draws a simple Mazda using cout.
+{
+    cout << "вrawing a Mazda" << endl;
+    cout << "     _______" << endl;
+    cout << " ___/ MAZDA \\___" << endl;
+    cout << "|  _       _   |" << endl;
+    cout << "'-(_)-----(_)--'" << endl;
+}
+
+void Mazda::drive() // This function drives the ьazda.
+{
+    cout << "Driving a Mazda" << endl;
+}
